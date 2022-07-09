19d2beed144e58da9cd6516f08e80e187eeb61fe
TsiamDev <kos.tsiamitros@gmail.com>
2022-07-09 09:23:30 +0300

    v 0.3

--- calculate_freqs.m
+++ calculate_freqs.m
@@ -1,4 +1,10 @@
-function [letters, freqs, tex] = calculate_freqs()
+function [letters, freqs, tex, counts] = calculate_freqs(f)
+    
+    if f == 1
+        freqs = readmatrix('frequencies.txt');
+        freqs = freqs(:,2);
+    end
+    
     tex = fileread('cvxopt-small.txt');
     letters = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';
         'l';'m';'n';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z';' '];
@@ -10,9 +16,11 @@ function [letters, freqs, tex] = calculate_freqs()
         counts(i) = sum_;
         total_letters = total_letters + sum_;
     end
-
-    freqs = zeros(27,1);
-    for i = 1:27
-        freqs(i) = counts(i) / total_letters;
+    
+    if f ~= 1
+        freqs = zeros(27,1);
+        for i = 1:27
+            freqs(i) = counts(i) / total_letters;
+        end
     end
 end
